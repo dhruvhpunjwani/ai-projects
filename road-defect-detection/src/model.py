b751b0f094781b7216c82a11b8c7ad8ad80e9c14
dhruvhpunjwani <dhruv.h.punjwani@gmail.com>
2026-03-25 03:40:35 +1100

    Create model.py

---
+++ road-defect-detection/src/model.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+from torchvision.models import resnet18, ResNet18_Weights
+
+
+def build_model():
+    model = resnet18(weights=ResNet18_Weights.DEFAULT)
+    in_features = model.fc.in_features
+
+    model.fc = nn.Sequential(
+        nn.Dropout(0.3),
+        nn.Linear(in_features, 2)
+    )
+
+    return model
